0a335cb9e6c7f2ade19ce0f165e637f724ba5ad5
hulijun1997 <1208476201@qq.com>
2018-12-27 10:46:41 +0800

    hlj

---
+++ src/main/java/com/example/demo3h/model/Role.java
@@ -0,0 +1,31 @@
+package com.example.demo3h.model;
+
+public class Role {
+    private int roleid;
+    private int uid;
+    private String rolename;
+
+    public int getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(int roleid) {
+        this.roleid = roleid;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+}
